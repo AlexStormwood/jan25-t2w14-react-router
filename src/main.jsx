44e3f5128b3a0723461fe1120b9c7402dadcbb95
Alex <alex.stormwood@bigfootds.com>
2026-05-04 19:35:33 +1000

    feat!: react app init with pages and navbar

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/Aboutpage'
+import ContactPage from './pages/ContactPage'
+import NavBar from './components/NavBar'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+
+    <BrowserRouter>
+      {/* <App /> */}
+      <NavBar />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path='about' element={<AboutPage />} />
+        <Route path='contact' element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
+
+    
+  </StrictMode>,
+)
